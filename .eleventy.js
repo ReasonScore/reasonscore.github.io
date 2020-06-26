f96f42f042df939b376bfcf01a20b1eae02542f1
Bentley Davis <Bentley@BentleyDavis.com>
2020-06-25 19:52:30 -0500

    blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "static-root": "/" });
   eleventyConfig.addPassthroughCopy({ "static-last": "/static" });
   eleventyConfig.addPassthroughCopy("static");
+
+  eleventyConfig.addPlugin(pluginRss);
+
   return {
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
     dir: {
       output: "dist"
     }
