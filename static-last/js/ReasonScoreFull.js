ac09212315ab4338205b09108ad17d262cb229eb
Bentley Davis <Bentley@BentleyDavis.com>
2021-01-18 09:35:49 -0600

    Correct build error in reasonscoreroot

--- static-last/js/ReasonScoreFull.js
+++
@@ -1,1 +0,0 @@
-document.body.appendChild(document.createElement('script')).src = (window.location.hostname !== "localhost"?"https://reasonscore.com":"") + `/static/js/ReasonScoreFull2.js?x=${new Date().getDate()}`;
