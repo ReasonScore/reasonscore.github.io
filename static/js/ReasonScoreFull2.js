4157990231dae2deea163d66f361853dd111c6df
Bentley Davis <Bentley@BentleyDavis.com>
2021-05-10 13:32:29 -0500

    Updates

--- static/js/ReasonScoreFull2.js
+++ static/js/ReasonScoreFull2.js
@@ -8,8 +8,8 @@
     ]
     const scripts2 = [
         
-        rootAddress + "/static/js/2.e50837ba.chunk.js",
-        rootAddress + "/static/js/main.c0dd3cfe.chunk.js",
+        rootAddress + "/static/js/2.2679d747.chunk.js",
+        rootAddress + "/static/js/main.cece241f.chunk.js",
     ]
 
     // default settings
@@ -30,15 +30,10 @@
 
     }
 
-    // Add Standard HTML and CSS
-    //if (rootAddress !== "") {
-        const r = document.createElement('div')
-        r.innerHTML = `
-        <link href="${rootAddress}/static/css/main.3f62a026.chunk.css" rel="stylesheet">
-
-    `;
-        document.body.appendChild(r);
-    //}
+    // Add Standard  CSS
+    const r = document.createElement('div')
+    r.innerHTML = `<link href="${rootAddress}/static/css/main.1f670247.chunk.css" rel="stylesheet">`;
+    document.body.appendChild(r);
 
     // Run any code that requires Javascript to have already been loaded and run 
     onload1 = function () {
