bd2ce5dd9db736925c58edb4d69a6ab72e73428d
Bentley Davis <Bentley@BentleyDavis.com>
2021-06-07 19:29:16 -0500

    correct tweet link color

--- static/js/ReasonScoreFull2.js
+++ static/js/ReasonScoreFull2.js
@@ -32,7 +32,7 @@
 
     // Add Standard CSS
     const r = document.createElement('div')
-    r.innerHTML = `<link href="${rootAddress}/static/css/main.d84a1403.chunk.css" rel="stylesheet">`;
+    r.innerHTML = `<link href="${rootAddress}/static/css/main.7bfa1767.chunk.css" rel="stylesheet">`;
     document.body.appendChild(r);
 
     // Run any code that requires Javascript to have already been loaded and run 
